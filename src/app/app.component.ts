48548873eee831236b17261ad499206598386e46
Yasuyuki Saito <yasuyuki.saito.2012@gmail.com>
2018-10-21 13:49:24 +0900

    標準スクロール実装

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,42 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <h1>Virtual Scrolling Sample</h1>
-    <div>
-      List Count: <input type="number" [(ngModel)]="listSize">
-    </div>
-    <div>
-      <div>
-        Virtual Scrolling
-      </div>
-      <div>
-        <button (click)="setVirtualList(listSize)">Apply</button>
-        <button (click)="setVirtualList(0)">Clear</button>
-      </div>
-      <div>
-        <cdk-virtual-scroll-viewport itemSize="20" class="example-viewport">
-          <div *cdkVirtualFor="let item of virtualList" class="example-item">{{ item }}</div>
-        </cdk-virtual-scroll-viewport>
-      </div>
-    </div>
-  `,
-  styles: [
-    `.example-viewport { width: 200px; height: 200px; border: 1px solid black; }`,
-    `.example-item { height: 20px; }`
-  ]
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
 
 // #region fields
 
-  /** */
+  /** list item size */
   listSize = 100000;
 
-  /** */
+  /** basic scroll list item */
   basicList: string[] = [];
 
-  /** */
+  /** virtual scroll list item */
   virtualList: string[] = [];
 
 // #endregion
@@ -45,7 +22,7 @@ export class AppComponent {
 // #region methods
 
   /**
-   *
+   * set basic scroll list item.
    * @param size List Size
    */
   setBasicList(size: number): void {
@@ -53,7 +30,7 @@ export class AppComponent {
   }
 
   /**
-   *
+   * set virtual scroll list item.
    * @param size List Size
    */
   setVirtualList(size: number): void {
@@ -61,7 +38,7 @@ export class AppComponent {
   }
 
   /**
-   *
+   * Create List
    * @param size List Size
    */
   private createList(size: number): string[] {
